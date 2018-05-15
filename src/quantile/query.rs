9db879e2b2a726136a23e6da38260dd183f803aa
Will Daly <will.e.daly@gmail.com>
2018-05-15 06:48:01 -0700

    Use binary search for query
    Reduces time to find 0.1 and 0.9 quantiles by an order of magnitude;
    reduces time to find median by two orders of magnitude!

--- src/quantile/query.rs
+++ src/quantile/query.rs
@@ -3,34 +3,32 @@ use std::cmp::{Ord, Ordering, PartialEq, PartialOrd};
 use std::iter::Iterator;
 
 #[derive(Copy, Clone, Eq)]
-struct WeightedValue {
+struct RankedValue {
     value: u64,
-    weight: usize,
+    rank: usize,
 }
 
-impl Ord for WeightedValue {
-    fn cmp(&self, other: &WeightedValue) -> Ordering {
+impl Ord for RankedValue {
+    fn cmp(&self, other: &RankedValue) -> Ordering {
         self.value.cmp(&other.value)
     }
 }
 
-impl PartialOrd for WeightedValue {
-    fn partial_cmp(&self, other: &WeightedValue) -> Option<Ordering> {
+impl PartialOrd for RankedValue {
+    fn partial_cmp(&self, other: &RankedValue) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl PartialEq for WeightedValue {
-    fn eq(&self, other: &WeightedValue) -> bool {
+impl PartialEq for RankedValue {
+    fn eq(&self, other: &RankedValue) -> bool {
         self.value == other.value
     }
 }
 
 pub struct SketchQuery {
     count: usize, // num items from the source data stream
-
-    // asc by value
-    sorted_items: Vec<WeightedValue>,
+    items: Vec<RankedValue>,  // sorted asc by value
 }
 
 impl SketchQuery {
@@ -41,59 +39,52 @@ impl SketchQuery {
             .sum();
         SketchQuery {
             count: count,
-            sorted_items: SketchQuery::sorted_items_from_sketch(sketch),
+            items: SketchQuery::sorted_items_from_sketch(sketch),
         }
     }
 
     pub fn query(&self, phi: f64) -> Option<u64> {
         assert!(0.0 < phi && phi < 1.0);
-
-        // Algorithm terminates early once it finds a value in the error bounds
-        // so if phi > 0.5 we reverse the order and search for (1 - phi)
-        // to find the quantile sooner.
-        if phi <= 0.5 {
-            SketchQuery::search_items(phi, self.count, self.sorted_items.iter())
+        let target = phi * self.count as f64;
+        let mut start = 0;
+        let mut end = self.items.len();
+        while end - start > 1 {
+            let mid = start + (end - start) / 2;
+            let rank = self.items[mid].rank as f64;
+            if target < rank {
+                end = mid;
+            } else if target > rank {
+                start = mid;
+            } else {
+                return Some(self.items[mid].value)
+            }
+        }
+        if end - start == 1 {
+            Some(self.items[start].value)
         } else {
-            SketchQuery::search_items(1.0 - phi, self.count, self.sorted_items.iter().rev())
+            None
         }
     }
 
-    fn search_items<'a, I>(phi: f64, n: usize, item_iter: I) -> Option<u64>
-    where
-        I: Iterator<Item = &'a WeightedValue>,
-    {
-        let error_bound = EPSILON * n as f64;
-        let target = phi * n as f64;
-        let mut closest: Option<(f64, u64)> = None;
-        let mut rank = 0;
-        for &WeightedValue { value, weight } in item_iter {
-            rank += weight;
-            let error = (rank as f64 - target).abs();
-            if error < error_bound {
-                return Some(value);
-            }
-
-            closest = match closest {
-                None => Some((error, value)),
-                Some((old_error, _)) if error < old_error => Some((error, value)),
-                Some(c) => Some(c),
-            }
-        }
-        closest.map(|(_, val)| val)
-    }
-
-    fn sorted_items_from_sketch(sketch: &Sketch) -> Vec<WeightedValue> {
+    fn sorted_items_from_sketch(sketch: &Sketch) -> Vec<RankedValue> {
         let mut items = Vec::with_capacity(BUFCOUNT * BUFSIZE);
         for b in sketch.buffer_iter() {
             let weight = 1 << b.level();
             for value in b.values() {
-                items.push(WeightedValue {
+                items.push(RankedValue {
                     value: *value,
-                    weight: weight,
+                    rank: weight,  // store the weight here for now to avoid extra allocation
                 })
             }
         }
         items.sort_unstable();
+
+        let mut rank = 0;
+        for x in items.iter_mut() {
+            let weight = x.rank;  // stored weight from earlier
+            x.rank = rank;
+            rank += weight;
+        }
         items
     }
 }
