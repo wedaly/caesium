49b0f90ac53eccf8b17e52f298fb45d4ff0cadb6
Will Daly <will.e.daly@gmail.com>
2018-09-04 19:53:22 -0700

    Refactor: use helper methods in query worker

--- caesium-load/src/worker/query.rs
+++ caesium-load/src/worker/query.rs
@@ -49,6 +49,50 @@ impl QueryWorker {
             tx,
         }
     }
+
+    fn write_until_done_or_blocked(
+        s: &mut TcpStream,
+        mut num_written: usize,
+        buf: &[u8],
+    ) -> Result<usize, io::Error> {
+        while num_written < buf.len() {
+            match s.write(&buf[num_written..]) {
+                Ok(n) => {
+                    num_written += n;
+                }
+                Err(err) => {
+                    if let io::ErrorKind::WouldBlock = err.kind() {
+                        break;
+                    } else {
+                        return Err(err);
+                    }
+                }
+            }
+        }
+        return Ok(num_written);
+    }
+
+    fn read_until_done_or_blocked(s: &mut TcpStream) -> Result<bool, io::Error> {
+        let mut buf = [0; 1024];
+        let mut is_done = false;
+        loop {
+            match s.read(&mut buf) {
+                Ok(n) if n == 0 => {
+                    is_done = true;
+                    break;
+                }
+                Ok(_) => {}
+                Err(err) => {
+                    if let io::ErrorKind::WouldBlock = err.kind() {
+                        break;
+                    } else {
+                        return Err(err);
+                    }
+                }
+            }
+        }
+        Ok(is_done)
+    }
 }
 
 impl Worker for QueryWorker {
@@ -83,23 +127,10 @@ impl Worker for QueryWorker {
     fn write(&mut self) -> Result<(), io::Error> {
         self.state = match self.state.take() {
             Some(State::Writing(mut s, mut num_written)) => {
-                let buf = self.queries[self.query_idx].as_bytes();
-                while num_written < buf.len() {
-                    match s.write(&buf[num_written..]) {
-                        Ok(n) => {
-                            num_written += n;
-                        }
-                        Err(err) => {
-                            if let io::ErrorKind::WouldBlock = err.kind() {
-                                break;
-                            } else {
-                                return Err(err);
-                            }
-                        }
-                    }
-                }
-
-                if num_written < buf.len() {
+                let query_buf = self.queries[self.query_idx].as_bytes();
+                num_written =
+                    QueryWorker::write_until_done_or_blocked(&mut s, num_written, query_buf)?;
+                if num_written < query_buf.len() {
                     Some(State::Writing(s, num_written))
                 } else {
                     s.shutdown(Shutdown::Write)?;
@@ -118,29 +149,10 @@ impl Worker for QueryWorker {
     fn read(&mut self) -> Result<(), io::Error> {
         self.state = match self.state.take() {
             Some(State::Reading(mut s)) => {
-                let mut buf = [0; 1024];
-                let mut is_done = false;
-                loop {
-                    match s.read(&mut buf) {
-                        Ok(n) if n == 0 => {
-                            is_done = true;
-                            break;
-                        }
-                        Ok(_) => {}
-                        Err(err) => {
-                            if let io::ErrorKind::WouldBlock = err.kind() {
-                                break;
-                            } else {
-                                return Err(err);
-                            }
-                        }
-                    }
-                }
-
+                let is_done = QueryWorker::read_until_done_or_blocked(&mut s)?;
                 self.tx
                     .send(Event::query_bytes_received_event(self.id, self.query_idx))
                     .expect("Could not send query bytes received event to channel");
-
                 if is_done {
                     self.query_idx = (self.query_idx + 1) % self.queries.len();
                     None
