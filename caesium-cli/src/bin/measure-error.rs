2d49cd05999f69e9b93926bd70874f16f559d725
Will Daly <will.e.daly@gmail.com>
2019-01-12 20:10:10 -0800

    Update measure error script

--- caesium-cli/src/bin/measure-error.rs
+++ caesium-cli/src/bin/measure-error.rs
@@ -6,11 +6,11 @@ use caesium_core::quantile::writable::WritableSketch;
 use rand::rngs::SmallRng;
 use rand::{FromEntropy, Rng};
 
-const NUM_INSERTS: usize = 1_000_000;
+const NUM_INSERTS: usize = 10_000;
 const NUM_TRIALS: usize = 10;
 
 fn main() {
-    println!("insert,trial,error");
+    println!("phi,trial,error");
     for t in 0..NUM_TRIALS {
         run_trial(t);
     }
@@ -18,17 +18,22 @@ fn main() {
 
 fn run_trial(trial: usize) {
     let mut rng = SmallRng::from_entropy();
+    let mut s = WritableSketch::new();
+    let mut data = Vec::with_capacity(NUM_INSERTS);
     for n in 1..NUM_INSERTS {
-        let mut s = WritableSketch::new();
-        let mut data = Vec::with_capacity(n);
         for _ in 0..n {
             let v: u32 = rng.gen();
             data.push(v);
             s.insert(v);
         }
-        let c = ErrorCalculator::new(&data);
-        let q = s.to_readable().query(0.5).unwrap().approx_value;
-        let e = c.calculate_error(0.5, q);
-        println!("{},{},{}", n, trial, e);
+    }
+    let c = ErrorCalculator::new(&data);
+    let r = s.to_readable();
+
+    for i in 1..10 {
+        let phi = (i as f64) / 10f64;
+        let q = r.query(phi).unwrap().approx_value;
+        let e = c.calculate_error(phi, q);
+        println!("{},{},{}", phi, trial, e);
     }
 }
